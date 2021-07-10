577c58fa537870d228adcbb0678969bb80de2c0a
jackpoit <jackpoit_able@outlook.com>
2021-07-10 09:32:08 +0800

    treeset and priorityQueue

--- src/heapandpriorityQueue/Solution.java
+++ src/heapandpriorityQueue/Solution.java
@@ -1,11 +1,8 @@
 package heapandpriorityQueue;
 
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 import java.util.PriorityQueue;
-
 
 
 public class Solution {
@@ -21,6 +18,7 @@ public class Solution {
 		PriorityQueue<Integer> queue=new PriorityQueue<>(
 				Comparator.comparing(map::get)
 		);
+
 		//这里有3种写法  1.直接lambda表达式  (o1,o2)->map.get(o1)-map.get(o1)   省略了参数类型、方法体 和return
 		//2. 用Comparator.comparing() 里面传一个键提取器  就是对要进行比较的o1 o2先执行括号中的操作 再compareTo
 		// 键提取器有2种写法  1. 用lambda表达式  a->map.get(a)  就是 o1 o2都先应用map.get(o1/o2) 再compareTo
@@ -29,6 +27,13 @@ public class Solution {
 		//如果是需要o1.getName() 因为getName不需要参数
 		// 但是lambda参数(o1,o2)作为隐式参数(就是调用者) 就要用类名::非静态方法(这样参数就可以作为调用者了)
 		//如  Employee类的比较   emp->emp.getName()  就可以简写成Employee::getName
+
+		//优先队列与TreeSet的区别
+		//优先队列允许重复元素(父元素大于等于子元素)   内部无序  应用 需要求最大或最小值的场景(就是本题 在入队时把优先级最小的要剔除）
+		//(内部是最大/最小堆基于数组实现的） (头和尾很好找)
+		//TreeSet去重(基于二叉树）   内部有序   应用需要找出集合中大于等于某个数值的最小值(ceiling/floor)
+		// 但找最大最小元素不好找(内部是树结构 头和尾不太好找)
+		//
 
 		for (int key:map.keySet()){
 			if (queue.size()<k){
