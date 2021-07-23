358e83715379b38e1eedd40601d9d5a683093590
jackpoit <jackpoit_able@outlook.com>
2021-07-23 13:12:14 +0800

    p547

--- src/UnionFind/SolutionP547.java
+++ src/UnionFind/SolutionP547.java
@@ -6,8 +6,125 @@ package UnionFind;
  * @Description:
  */
 public class SolutionP547 {
+	private interface UF {
+		/**
+		 * getSize
+		 *
+		 * @return size
+		 */
+		int getSize();
+
+		/**
+		 * @param p 元素索引
+		 * @param q 元素索引
+		 * @return 是否在一个集合中
+		 */
+		boolean isConnected(int p, int q);
+
+		/**
+		 * 合并2个元素到集合
+		 * @param p 元素索引
+		 * @param q 元素索引
+		 */
+		void unionElements(int p, int q);
+	}
+	private class UnionFind6 implements UF {
+
+		private int[] parent;    // parent[i]表示第i个元素指向的父节点
+		private int[] rank;
+		// rank[i]表示以i为根的集合所表示的树的层数
+		// 在后续的代码中, 我们并不会维护rank的语意, 也就是rank的值在路径压缩的过程中, 有可能不在是树的层数值
+		// 这也是我们的rank不叫height或者depth的原因, 他只是作为比较的一个标准
 
 
+		public UnionFind6(int size) {
+			parent = new int[size];
+			rank = new int[size];
+			//初始化每个元素父节点指向自己 每一个元素都是一个根元素
+			for (int i = 0; i < size; i++) {
+				parent[i] = i;
+				rank[i] = 1;
+			}
+		}
 
+		@Override
+		public int getSize() {
+			return parent.length;
+		}
 
+		// 查看元素p和元素q是否所属一个集合
+		// O(h)复杂度, h为树的高度
+		@Override
+		public boolean isConnected(int p, int q) {
+			return find(p) == find(q);
+		}
+
+		//找到p元素的根节点 即p元素对应的集合编号
+		//时间复杂度O(h) h为树的高度
+		//找到并返回p的父节点(迭代到最后实际返回的是p的根节点)
+		private int find(int p) {
+			if (p < 0 || p >= parent.length) {
+				throw new IllegalArgumentException("p is out of bound");
+			}
+
+			if(parent[p] != p) {
+				parent[p]=find(parent[p]);
+			}
+			// 如果p不是根节点 就让p指向p的父节点(下一节点)的父节点
+			// 通过find(parent[p])得到  不能写find(p)会无限迭代
+			//当最后相等了 说明是根节点了 就停止了迭代
+			return parent[p];
+		}
+		//这样find 1次:会使当前节点(p)-->到其根节点之间的所有节点都指向根节点
+
+		//合并元素p,q的所属集合
+		//时间复杂度O(h) h为树的高度
+		@Override
+		public void unionElements(int p, int q) {
+			int pRoot = find(p);
+			int qRoot = find(q);
+			//找到p q的根节点
+			if (pRoot == qRoot) {
+				return;
+			}
+
+			//根据2个元素根元素的rank值判断合并方向
+			//将rank值低的合并到rank值高的上面
+			if (rank[pRoot] > rank[qRoot]) {
+				parent[qRoot] = pRoot;
+			} else if (rank[pRoot] < rank[qRoot]) {
+				parent[pRoot] = qRoot;
+			} else { //rank[pRoot] = rank[qRoot]
+				parent[qRoot]=pRoot;
+				rank[pRoot]+=1;
+			}
+		}
+
+		public int findRootSize(){
+			int res=0;
+			for (int i = 0; i < parent.length; i++) {
+				if (parent[i]==i)
+					res+=1;
+			}
+			return res;
+		}
+	}
+
+	public int findCircleNum(int[][] isConnected) {
+			int size=isConnected.length;
+			if (size==0){
+				return 0;
+			}
+			UnionFind6 unionFind6=new UnionFind6(size);
+			for (int i=0;i<size;i++){
+				for (int j=0;j<i;j++){
+					if (isConnected[i][j]==1){
+						unionFind6.unionElements(i,j);
+					}
+				}
+			}
+
+			return unionFind6.findRootSize();
+
+	}
 }
