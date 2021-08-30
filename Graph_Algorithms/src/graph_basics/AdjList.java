22efadc68df8ee3992e3a8ce308519f96fe49bfc
jackpoit <jackpoit_able@outlook.com>
2021-08-30 10:04:35 +0800

    basics graph

---
+++ Graph_Algorithms/src/graph_basics/AdjList.java
@@ -0,0 +1,107 @@
+package graph_basics;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @Author: jackpoit
+ * @Date: 2021/08/26/13:46
+ * @Description:
+ */
+public class AdjList {
+	private int V;
+	private int E;
+	private LinkedList<Integer>[] adj;
+
+	public AdjList(String filename) {
+
+		File file = new File(filename);
+
+		try (Scanner scanner = new Scanner(file)) {
+			V = scanner.nextInt();
+			if (V < 0)
+				throw new IllegalArgumentException("V must be non-negative");
+			adj = new LinkedList[V];
+
+			for (int i = 0; i < V; i++) {
+				adj[i]=new LinkedList<>();
+			}
+
+			E = scanner.nextInt();
+			if (E < 0)
+				throw new IllegalArgumentException("E must be non-negative");
+
+			for (int i = 0; i < E; i++) {
+				int a = scanner.nextInt();
+				validateVertex(a);
+				int b = scanner.nextInt();
+				validateVertex(b);
+
+				if (a == b)
+					throw new IllegalArgumentException("Self Loop is Detected");
+
+				if (adj[a].contains(b))
+					throw new IllegalArgumentException("Parallel Edges are Detected");
+
+				adj[a].add(b);
+				adj[b].add(a);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void validateVertex(int v) {
+		if (v < 0 || v >= V)
+			throw new IllegalArgumentException("vertex" + v + "is invalid");
+	}
+
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+	//v与w是否相连
+	public boolean hasEdge(int v, int w) {
+		validateVertex(v);
+		validateVertex(w);
+		return adj[v].contains(w);
+	}
+
+	//与v相连的点的集合
+	public Iterable<Integer> adj(int v) {
+		validateVertex(v);
+		return adj[v];
+	}
+
+	public int degree(int v) {
+		validateVertex(v);
+		return adj[v].size();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(String.format("V = %d, E = %d \n", V, E));
+		for (int v = 0; v < V; v++) {
+			sb.append(String.format("%d :",v));
+			for (int w :adj[v]) {
+				sb.append(String.format("%d ",w));
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		AdjList adjList = new AdjList("g.txt");
+		System.out.println(adjList);
+	}
+}
